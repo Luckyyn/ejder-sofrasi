54ed3f9dd05ab4baf9f9c3a634da34c980cfa517
Yusuf <yusufugur2337@gmail.com>
2026-07-05 01:44:39 +0300

    Dialect turso olarak guncellendi

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'drizzle-kit';
 export default defineConfig({
     schema: './src/db/schema.ts',
     out: './drizzle',
-    dialect: 'sqlite',
+    dialect: 'turso', // İşte kilit nokta burası! 'sqlite' yerine 'turso' yazıyoruz.
     dbCredentials: {
         url: process.env.DATABASE_URL!,
         authToken: process.env.DATABASE_AUTH_TOKEN!,
